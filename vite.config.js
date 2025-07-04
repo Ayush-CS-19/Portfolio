e6c78fa9b65b5d6c4cdf401058d5cb7f0172da2a
Ayush-CS-19 <ayushkaushik257@gmail.com>
2025-07-04 17:00:11 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import autoprefixer from "autoprefixer";
-import postcss from "postcss";
-import { a } from "framer-motion/client";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), autoprefixer(), postcss()],
+  plugins: [react()],
+  css: {
+    postcss: "./postcss.config.js",
+  },
   assetsInclude: ["**/*.glb"],
 });
